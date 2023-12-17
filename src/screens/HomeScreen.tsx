b301787a96ccc86ab6342b4e1262cf48d61269f3
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-17 12:17:55 -0300

    fazendo login, armazenando o token e depois chamando um endpoint autenticado sem precisar passar o token manualmente

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import api from '../api/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen: React.FC = () => {
+  const handleLogin = async () => {
+    try {
+      const response = await api.post('/ong/login', {name: 'lucas', email: 'lucas', password: 'lucas' });
+      const token = response.data;
+
+      // console.log('Resposta do login:', response);
+      console.log('Token HomeScreen:', token);
+  
+      await AsyncStorage.setItem('authToken', token);
+  
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } 
+    catch (error) {
+      console.error('Erro no login:', error);
+    }
+  };
+
+  useEffect(() => {
+    handleLogin();
+    
+  }, []);
+  
   const [pingResponse, setPingResponse] = useState<string | null>(null);
 
   useEffect(() => {

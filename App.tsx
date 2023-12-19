a36dc44dda6c837d5d118d5cd61bfd8446200d31
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-19 18:05:27 -0300

    importando telas e mostrando o statusbar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
+import { WelcomeScreen, LoginScreen } from './src/screens';
+import SignupScreen from './src/screens/SignupScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <StatusBar />
+      <Stack.Navigator
+        initialRouteName="WelcomeScreen"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;

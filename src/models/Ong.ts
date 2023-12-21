ab1e16478849dab02f4b02f7ad866b76065db967
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-21 08:18:07 -0300

    removendo export default da ong

--- src/models/Ong.ts
+++ src/models/Ong.ts
@@ -2,12 +2,12 @@ class Ong {
   public name: string;
   public email: string;
   public password: string;
+  public cnpj: string;
   
-  constructor(name: string, email: string, password: string) {
+  constructor(name: string, email: string, password: string, cnpj: string) {
     this.name = name;
     this.email = email;
     this.password = password;
+    this.cnpj = cnpj;
   }
 }
-
-export default Ong;

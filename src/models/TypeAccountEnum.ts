3426ea7ddd65bf408d3d131ea8ba61132dcedb3a
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2023-12-21 08:12:29 -0300

    espelhando junto ao back a classe User

---
+++ src/models/TypeAccountEnum.ts
@@ -0,0 +1,4 @@
+enum TypeAccountEnum {
+  VOLUNTEER,
+  ONG
+}
---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+class User {
+  public name: string;
+  public email: string;
+  public password: string;
+  public typeAccountEnum: TypeAccountEnum;
+
+  // Volunteer
+  public phone?: string;
+
+  // Ong
+  public cnpj?: string;
+
+  constructor(name: string, email: string, password: string, typeAccountEnum: TypeAccountEnum, phone?: string, cpnj?: string) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.typeAccountEnum = typeAccountEnum;
+    this.phone = phone;
+    this.cnpj = cpnj;
+  }
+
+}

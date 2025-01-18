f063a076d634db2ec505b0bcdebea4f7cc5ebaa4
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-18 16:10:49 -0300

    🎨♻️ (LoginScreen): Refatorando

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,70 +1,103 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import api from '../api/api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import User from '../models/User';
-import TypeAccountEnum from '../utils/TypeAccountEnum';
+import Layout from '../components/Layout';
+import COLORS from '../constants/colors';
 
-const LoginScreen: React.FC = ({ navigation }) => {
+const LoginScreen: React.FC = ({ navigation }: any) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
   const handleLogin = async () => {
     const user = new User(email, password);
 
     try {
-      console.log(JSON.stringify(user));
       const response = await api.post('/user/getToken', user);
       
       const token = response.data;
 
-      console.log('Token LoginScreen:', token);  
-  
       await AsyncStorage.setItem('authToken', token);
   
       api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       navigation.navigate('HomeScreen');
-      
     } 
     catch(error) {
       console.error('Erro no login:', error);
     }
-
   };
-  
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
     
   return(
-    <View style={styles.container}>
-      <View style={styles.inputs}>
-        <Input placeholder="Email" onChange={(value) => setEmail(value)} />
-        <Input placeholder="Senha" secureTextEntry={true} onChange={(value) => setPassword(value)} />
+    <Layout navigation={navigation}>
+      <View style={styles.titleView}>
+        <Text style={styles.title}>
+          Que bom ter você de volta!
+        </Text>
+      </View>
+      
+      <View style={styles.main}>
+        <View style={styles.inputs}>
+          <Input placeholder="Email" onChange={(value) => setEmail(value)} keyboardType="email-address" />
+          <Input placeholder="Senha" secureTextEntry={true} onChange={(value) => setPassword(value)} />
+        </View>
       </View>
 
-      <View style={styles.button}>
-        <Button title="Login" onPress={handleLogin} />
-      </View>
+      <View style={styles.actions}>
+        <Button
+          title="Login"
+          onPress={handleLogin}
+        />
 
-    </View>
+        <View style={styles.actionsForgotPassword}>
+          <Text>Esqueceu a senha?</Text>
+          <Text style={styles.forgotPasswordText} onPress={() => navigation.navigate('ForgotPasswordScreen')}>
+            Recupere aqui
+          </Text>
+        </View>
+      </View>
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  titleView: {
+    width: '100%',
   },
-  inputs: {
+  
+  title: {
+    fontFamily: 'Casual',
+    fontSize: 44,
+    color: COLORS.primary,
+  },
+
+  main: {
     width: '100%',
     gap: 15,
+    justifyContent: 'space-around',
+    height: '65%',
   },
-  button: {
-    marginTop: 20,
-    width: '80%',
+
+  inputs: {
     gap: 15,
-  }
+  },
+
+  actions: {
+    width: '100%',
+  },
+
+  actionsForgotPassword: {
+    marginTop: 8,
+    alignItems: 'center',
+  },
+
+  forgotPasswordText: {
+    color: COLORS.primary,
+    fontWeight: '700',
+  },
 });
 
 export default LoginScreen;

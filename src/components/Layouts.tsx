15482645926fc1c8f47512175b44f4cd0f135421
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-18 11:03:55 -0300

    ✨ (Layout): Add uma lista de telas onde o BottomNavigation não irá aparecer

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
+import { NavigationProp, useRoute } from '@react-navigation/native';
 import { handleTabNavigation } from '../utils/navigateUtil';
 import BottomNavigation from './BottomNavigation';
 
@@ -10,13 +10,19 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children, navigation }) => {
+  const noBottomNavigationScreens = ['LoginScreen', 'SignupScreen'];
+  const route = useRoute();
+  const isBottomNavigationVisible = !noBottomNavigationScreens.includes(route.name);
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
         {children}
       </View>
       
-      <BottomNavigation onTabPress={(tabName) => handleTabNavigation(tabName, navigation)} />
+      {isBottomNavigationVisible && (
+        <BottomNavigation onTabPress={(tabName) => handleTabNavigation(tabName, navigation)} />
+      )}
     </View>
   );
 };

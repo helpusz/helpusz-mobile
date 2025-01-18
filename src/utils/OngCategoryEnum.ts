5c38a23bb4e3daa21157b8af48bb1fb4b221666f
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-18 10:00:45 -0300

    ✨ (OngCategoryEnum): Criando enum para as categorias das Ongs

---
+++ src/utils/OngCategoryEnum.ts
@@ -0,0 +1,24 @@
+enum OngCategoryEnum {
+  MEIO_AMBIENTE = "Meio Ambiente",
+	CAUSA_ANIMAL = "Causa Animal",
+	EDUCACAO = "Educação",
+	SAUDE = "Saúde",
+	ASSISTENCIA_SOCIAL = "Assistência Social",
+	DIREITOS_HUMANOS = "Direitos Humanos",
+	CULTURA_E_ARTES = "Cultura e Artes",
+	DESENVOLVIMENTO_COMUNITARIO = "Desenvolvimento Comunitário",
+	ESPORTE_E_LAZER = "Esporte e Lazer",
+	HABITACAO = "Habitação",
+	TECNOLOGIA_E_INOVACAO = "Tecnologia e Inovação",
+	SEGURANCA_ALIMENTAR = "Segurança Alimentar",
+	IGUALDADE_RACIAL = "Igualdade Racial",
+	IGUALDADE_DE_GENERO = "Igualdade de Gênero",
+	DEFESA_DOS_DIREITOS_INFANTOJUVENIS = "Defesa dos Direitos Infantojuvenis",
+	DIREITOS_DOS_IDOSOS = "Direitos dos Idosos",
+	INCLUSAO_DE_PESSOAS_COM_DEFICIENCIA = "Inclusão de Pessoas com Deficiência",
+	PROTECAO_CIVIL = "Proteção Civil",
+	REFUGIADOS_E_MIGRANTES = "Refugiados e Migrantes",
+	JUSTICA_E_CIDADANIA = "Justiça e Cidadania"
+}
+
+export default OngCategoryEnum;

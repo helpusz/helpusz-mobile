cfa468c6eaa83b82fe38985577c7ebc8f77a767c
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-22 10:31:26 -0300

    ✨ (SocialLinks): Permitindo link de sites

--- src/utils/SocialLinks.ts
+++ src/utils/SocialLinks.ts
@@ -3,12 +3,14 @@ class SocialLinks {
   public facebook: string;
   public instagram: string;
   public linkedin: string;
+  public site: string
 
-  constructor(twitter: string, facebook: string, instagram: string, linkedin: string) {
+  constructor(twitter: string, facebook: string, instagram: string, linkedin: string, site: string) {
     this.twitter = twitter;
     this.facebook = facebook;
     this.instagram = instagram;
     this.linkedin = linkedin;
+    this.site = site;
   }
 }
 

7608e905d38fba78fdcda2c74787ebbe147bed95
Lucas Gomes da Silva <silva.lucasgomes@protonmail.com>
2025-01-22 10:34:37 -0300

    ♻️ (User): Alterando tipo do email

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,12 @@
 import TypeAccountEnum from "../utils/TypeAccountEnum";
 import OngCategoryEnum from "../utils/OngCategoryEnum";
 import SocialLinks from "../utils/SocialLinks";
+import Email from "../utils/Email";
 
 export interface User {
   id?: string;
   name?: string;
-  email?: string;
+  email?: Email;
   password: string;
   typeAccount?: TypeAccountEnum;
   socialLinks?: SocialLinks;
